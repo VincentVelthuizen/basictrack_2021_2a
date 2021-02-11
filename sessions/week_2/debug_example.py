b80fca423d76731b877bee67156ba2864d224d73
Vincent Velthuizen <mail@vincentvelthuizen.com>
2021-02-11 21:17:42 +0100

    second half of session 2

--- sessions/week_2/debug_example.py
+++ sessions/week_2/debug_example.py
@@ -1,13 +1,15 @@
 current_time_str = input("What is the current time (in hours 0-23)?")
-wait_time_str = input("How many hours do you want to wait")
+# wait_time_str = input("How many hours do you want to wait")
 
 current_time_int = int(current_time_str)
-wait_time_int = int(wait_time_str)
+# wait_time_int = int(wait_time_str)
 
-final_time_int = current_time_int + wait_time_int
+wait_time = int(input("How many hours do you need to wait? "))
+
+final_time_int = current_time_int + wait_time
 
 number_of_days = final_time_int / 24
 waiting_days = final_time_int // 24
 final_answer = final_time_int % 24
 
-print("The time after waiting is: ", final_answer)
+print("The time after waiting is:", final_answer)
