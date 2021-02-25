1ecd3ed1f9761989ab94d11fce9dca4c1124bbd1
Vincent Velthuizen <mail@vincentvelthuizen.com>
2021-02-25 14:54:05 +0100

    week 3 session

--- how_to_think/chapter_2/exercise_2_14_7.py
+++ how_to_think/chapter_2/exercise_2_14_7.py
@@ -2,3 +2,6 @@ current_time = 2 + 12   # plus 12 because pm
 alarm_remaining = 51
 alarm_goes_off = (current_time + alarm_remaining) % 24
 print("The alarm will go off at", alarm_goes_off)
+
+
+# 5/0 1 2 3 4 5/0
---
+++ sessions/week_3/teenage_mutant.py
@@ -0,0 +1,21 @@
+import turtle
+
+ninja = turtle.Turtle()
+screen = turtle.Screen()
+
+screen.title("Teenage Mutant Ninja Turtles")
+screen.bgcolor("papayawhip")
+
+for step in range(15):
+    ninja.forward(50)
+    ninja.left(120)
+
+    # after a complete triangle
+    print(step, step % 3)
+    if step % 3 == 2:
+        ninja.penup()
+        ninja.forward(100)
+        ninja.pendown()
+
+
+screen.exitonclick()
